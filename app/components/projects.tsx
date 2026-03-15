449bc858d8abfcefeaab6d99dc1dce313c3a2f7e
Achmad Ilmi / Terence <dznilmi@gmail.com>
2026-03-15 13:19:06 +0700

    feat: Add ProjectPosts component to display sorted project blog posts

---
+++ app/components/projects.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { formatDate } from "app/blog/utils";
+import { getProjectPosts } from "../project/utils";
+
+export function ProjectPosts() {
+  let allBlogs = getProjectPosts();
+
+  return (
+    <div>
+      {allBlogs
+        .sort((a, b) => {
+          if (
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post) => (
+          <Link
+            key={post.slug}
+            className="flex flex-col space-y-1 mb-4"
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full grid grid-cols-4 md:flex-row space-x-0 md:space-x-2">
+              <p className="text-muted-foreground tabular-nums">
+                {formatDate(post.metadata.publishedAt, false)}
+              </p>
+              <p className="tracking-tight col-span-3">{post.metadata.title}</p>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
+}
